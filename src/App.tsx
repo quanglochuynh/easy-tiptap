6586fb204a94995133359d5ce48ba3bed7eef698
Loc Q Huynh <quanglochuynh@gmail.com>
2023-12-07 21:05:07 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-container">
+      <h1>Hello, world! You clicked the button.</h1>
+    </div>
+  );
+}
+
+export default App;
